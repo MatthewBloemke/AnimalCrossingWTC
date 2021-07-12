c37e3e9352fbcef7193a8c332f0fc23fd470af2b
Matthew <mattbloemke@gmail.com>
2021-07-12 02:28:22 -0500

    added a function to format 24 hour to 12 hour

--- src/server/utils/date-time.js
+++ src/server/utils/date-time.js
@@ -11,6 +11,19 @@ function asTimeString(date) {
     return date.getHours() + ":" + date.getMinutes();
 };
 
+export function asTwelveHourTimeString(time) {
+    if (time.slice(0,2) === "12") {
+        return time + "pm"
+    } else if (time.slice(0,2) > 12) {
+        console.log(time.slice(0,2))
+        const newTime = time.slice(0,2)-12
+        return `${newTime}:${time.slice(3,5)}pm` 
+    } else {
+        console.log(time)
+        return time + "am";
+    }
+}
+
 export function currentMonth() {
     const date = new Date()
     return (date.getMonth() + 1)
