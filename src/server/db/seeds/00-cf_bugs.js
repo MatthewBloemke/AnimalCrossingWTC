3f4438f7eb4928c3a5bb731d863218b8e495bd8c
Matthew <mattbloemke@gmail.com>
2021-07-11 22:20:13 -0500

    Removed knex and moved db

--- src/server/db/seeds/00-cf_bugs.js
+++
@@ -1,8 +0,0 @@
-const bugs = require("../../../../db/seeds/00-cf_bugs.json")
-
-exports.seed = function(knex) {
-  return knex.raw('TRUNCATE TABLE cf_bugs RESTART IDENTITY CASCADE')
-    .then(function () {
-      return knex("cf_bugs").insert(bugs)
-    })
-};
