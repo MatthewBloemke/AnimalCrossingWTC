bf20a82db5e73aa14f8a06aee0e9a6f703c39927
Matthew <mattbloemke@gmail.com>
2021-07-12 02:27:45 -0500

    Removed cors, as there will not be any backend api

--- src/server/app.js
+++ src/server/app.js
@@ -2,14 +2,11 @@ const path = require("path");
 require("dotenv").config({path: path.join(__dirname, "..", ".env")});
 
 const express = require("express");
-const cors = require("cors");
 const notFound = require("./errors/notFound")
 const errorHandler = require("./errors/errorHandler")
 
 const app = express();
 
-
-app.use(cors());
 app.use(express.json());
 
 app.get("/bundle.js", (req, res) => {
