9a42982091c19e0e7ca15b8089b03d8a8d465cc0
Matthew <mattbloemke@gmail.com>
2021-07-12 02:26:41 -0500

    Formats bug tables to display

---
+++ src/client/layout/formatInfoTables.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { asTwelveHourTimeString } from '../../server/utils/date-time';
+
+export function formatBugTable (bugs) {
+    const formattedTable = []
+    console.log(bugs)
+    for (let i = 0; i<bugs.length; i++) {
+        let timeRange =""
+        if (bugs[i].catch_time_start === "0:00") {
+            timeRange = "All day"
+        } else {
+            timeRange = `${asTwelveHourTimeString(bugs[i].catch_time_start)} - ${asTwelveHourTimeString(bugs[i].catch_time_end)}`
+        }
+        formattedTable.push(
+            <tr>
+                <td>{bugs[i].bug_name}</td>
+                <td>Image coming soon</td>
+                <td>{bugs[i].price}</td>
+                <td>{bugs[i].location}</td>
+                <td>{timeRange}</td>
+                <td>{bugs[i].months_str}</td>
+            </tr>
+        )
+    }
+    console.log(formattedTable)
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Bug Name</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Location</th>
+                    <th>Time</th>
+                    <th>Months</th>
+                </tr>                
+            </thead>
+            <tbody>
+                {formattedTable}
+            </tbody>
+        </table>
+    )
+}
