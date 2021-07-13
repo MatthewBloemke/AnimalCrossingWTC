11eeac7ef7f996313b30c66ab15e738902cea300
Matthew <mattbloemke@gmail.com>
2021-07-12 23:38:32 -0500

    added sorting, removed console logs

--- src/client/CityFolkDash.jsx
+++ src/client/CityFolkDash.jsx
@@ -6,17 +6,16 @@ const CityFolkDash = ({date, time, bugs, month}) => {
     const bugsFilteredByMonth = bugs.filter(bug => bug.months_array.includes(month))
     const [filteredBugs, setFilteredBugs] = useState([]);
     const [bugTable, setBugTable] = useState([]);
-    
-    const clicked = () => {
-        console.log(asTwelveHourTimeString("11:50"))
+    const [filterCondition, setFilterCondition] = useState("price")
+    const onChange = (event) => {
+        setFilterCondition(event.target.value)
     }
 
     useEffect(() => {
-        filterBugsByTime(bugsFilteredByMonth)
-    }, [])
+        filterBugsByTime(bugsFilteredByMonth, filterCondition)
+    }, [filterCondition])
 
-    const filterBugsByTime = (bugs) => {
-        console.log("time to filter")
+    const filterBugsByTime = (bugs, filterBy) => {
         const tempBugArr = [];
         for (let i = 0; i < bugs.length; i++) {
             if (bugs[i].catch_time_end > bugs[i].catch_time_start) {
@@ -29,18 +28,27 @@ const CityFolkDash = ({date, time, bugs, month}) => {
                 }
             }
         }
-        tempBugArr.sort((a, b) => b.price - a.price)
+        if (filterBy === "price") {
+            tempBugArr.sort((a, b) => b[filterBy] - a[filterBy])  
+        } else {
+            tempBugArr.sort((a, b) => {
+                return a[filterBy].toLowerCase().localeCompare(b[filterBy].toLowerCase())
+            })
+        }
         setBugTable(formatBugTable(tempBugArr))
         setFilteredBugs(tempBugArr)
     }
 
-    console.log("filtered", filteredBugs)
     return (
         <div>
             <h1>City Folk</h1>
             <p>{date}, {time}, {bugs[0].bug_name}</p>
+            <select name="filter" id="filter" onChange={onChange}>
+                <option value="price">Price</option>
+                <option value="location">Location</option>
+                <option value="bug_name">Bug name</option>
+            </select>
             {bugTable}
-            <button onClick={clicked}>Button</button>
         </div>
     )
 }
