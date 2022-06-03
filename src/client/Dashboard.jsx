d3a05e9ed64cb65e08af0a74d1e320c59685ceb7
MatthewBloemke <mattbloemke@gmail.com>
2022-06-03 03:01:57 +0000

    added mui

--- src/client/Dashboard.jsx
+++ src/client/Dashboard.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import {Link} from "react-router-dom"
-import {AppBar, Grid, IconButton, Toolbar, Typography, MenuItem, Menu, Drawer, useMediaQuery, Avatar, Divider, Button} from '@mui/material';
+import {Grid, Button} from '@mui/material';
 
 
 const Dashboard = () => {
 
     return (
         <main>
-            <Grid container justifyContent="center" >
-                <Link style={{textDecoration: "none"}} to="/cityfolk"><Button sx={{marginBottom: "15px", marginRight: "15px"}} variant="contained">Animal Crossing: City Folk</Button></Link>
-                <Link to="/newhorizons/northern" style={{textDecoration: "none"}}><Button variant="contained">Animal Crossing: New Horizons</Button></Link>             
+            <Grid container textAlign="center" sx={{mt: "15px"}}>
+                <Grid item xs={12} sm={6} md= {6} lg = {6}>
+                    <Link style={{textDecoration: "none", marginBottom: "15px"}} to="/cityfolk"><Button variant="contained">Animal Crossing: City Folk</Button></Link>
+                </Grid>
+                <Grid item xs={12} sm={6} md= {6} lg = {6}>
+                    <Link to="/newhorizons/northern" style={{textDecoration: "none"}}><Button variant="contained">Animal Crossing: New Horizons</Button></Link>
+                </Grid>
+                           
             </Grid>            
         </main>
     )
