1b1a4c4b61cdf0f400aa62b97d90f7b352db5173
MatthewBloemke <mattbloemke@gmail.com>
2022-06-03 03:00:26 +0000

    changed theme

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import CityFolkDash from './CityFolkDash';
-import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import {today, now, currentMonth} from "../server/utils/date-time"
-import NewHorizonsDash from './NewHorizonsDash';
-const cf_bugs = require("../server/db/00-cf_bugs.json")
-const cf_fish = require("../server/db/01-cf_fish.json")
-const nh_n_fish = require("../server/db/02-nh_n_fish.json")
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import Layout from './layout/Layout';
 
@@ -13,17 +7,8 @@ import Layout from './layout/Layout';
 const theme = createTheme({
     palette: {
         primary: {
-            main: "#1976d2",
-            light: '#63a4ff',
-            dark: '#004ba0',
-            contrastText: "#fff"
+            main: "#27df19",
         },
-        secondary: {
-            light: '#ab4fff',
-            main: '#7517d1',
-            dark: '#3c009f',
-            contrastText: '#fff',
-        }
     },
 })
 
