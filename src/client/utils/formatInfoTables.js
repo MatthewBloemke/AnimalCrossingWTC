6683083f372237003977a1d8e3369a5e45dcc2d1
MatthewBloemke <mattbloemke@gmail.com>
2022-06-03 03:03:18 +0000

    added mui DataGrid

--- src/client/utils/formatInfoTables.js
+++ src/client/utils/formatInfoTables.js
@@ -4,12 +4,12 @@ import { asTwelveHourTimeString } from '../../server/utils/date-time';
 
 export function formatBugTable (bugs) {
     const columns = [
-        {field: 'bug_name', headerName: 'Bug Name', width: 150},
-        {field: 'image', headerName: 'Image', width: 150},
-        {field: 'price', headerName: 'Price', width: 150},
-        {field: 'location', headerName: 'Location', width: 150},
-        {field: 'timeRange', headerName: 'Time', width: 150},
-        {field: 'months', headerName: 'Months', width: 150},
+        {field: 'bug_name', headerName: 'Bug Name', minWidth: 115, flex: 2},
+        {field: 'image', headerName: 'Image', minWidth: 60, flex: 2},
+        {field: 'price', headerName: 'Price', minWidth: 65, flex: 2},
+        {field: 'location', headerName: 'Location', minWidth: 185, flex: 2},
+        {field: 'timeRange', headerName: 'Time', minWidth: 140, flex: 2},
+        {field: 'months', headerName: 'Months', minWidth: 190, flex: 2},
     ]
     const rows = []
     for (let i = 0; i < bugs.length; i++) {
@@ -21,14 +21,6 @@ export function formatBugTable (bugs) {
         };
         rows.push(
             {id: bugs[i].bug_name, bug_name: bugs[i].bug_name, image: "Image Coming Soon", price: bugs[i].price, location: bugs[i].location, timeRange: timeRange, months: bugs[i].months_str}
-            // <tr key={bugs[i].bug_name}>
-            //     <td>{bugs[i].bug_name}</td>
-            //     <td>Image coming soon</td>
-            //     <td>{bugs[i].price}</td>
-            //     <td>{bugs[i].location}</td>
-            //     <td>{timeRange}</td>
-            //     <td>{bugs[i].months_str}</td>
-            // </tr>
         )
     }
     return (
@@ -36,6 +28,8 @@ export function formatBugTable (bugs) {
             <DataGrid
                 rows={rows}
                 columns={columns}
+                pageSize={50}
+                rowsPerPageOptions={[50]}
             />
         </div>
     )
@@ -43,13 +37,13 @@ export function formatBugTable (bugs) {
 
 export function formatFishTable (fish) {
     const columns = [
-        {field: 'fish_name', headerName: 'Fish Name', width: 150},
-        {field: 'image', headerName: 'Image', width: 150},
-        {field: 'shadow_size', headerName: "Shadow Size", width: 150},
-        {field: 'price', headerName: 'Price', width: 150},
-        {field: 'location', headerName: 'Location', width: 150},
-        {field: 'timeRange', headerName: 'Time', width: 150},
-        {field: 'months', headerName: 'Months', width: 150},
+        {field: 'fish_name', headerName: 'Fish Name', minWidth: 115, flex: 2},
+        {field: 'image', headerName: 'Image', minWidth: 60, flex: 2},
+        {field: 'price', headerName: 'Price', minWidth: 65, flex: 2},
+        {field: 'shadow_size', headerName: "Shadow Size", minWidth: 120, flex: 2},
+        {field: 'location', headerName: 'Location', minWidth: 185, flex: 2},
+        {field: 'timeRange', headerName: 'Time', minWidth: 140, flex: 2},
+        {field: 'months', headerName: 'Months', minWidth: 190, flex: 2},
     ]
     const rows = []
     for (let i = 0; i<fish.length; i++) {
@@ -60,16 +54,7 @@ export function formatFishTable (fish) {
             timeRange = `${asTwelveHourTimeString(fish[i].catch_time_start)} - ${asTwelveHourTimeString(fish[i].catch_time_end)}`
         }
         rows.push(
-            {id: fish[i].fish_name, fish_name: fish[i].fish_name, image: "Image Coming Soon", price: fish[i].price, location: fish[i].location, timeRange: timeRange, months: fish[i].months_str}
-            // <tr key={fish[i].fish_name}>
-            //     <td>{fish[i].fish_name}</td>
-            //     <td>Image coming soon</td>
-            //     <td>{fish[i].price}</td>
-            //     <td>{fish[i].shadow_size}</td>
-            //     <td>{fish[i].location}</td>
-            //     <td>{timeRange}</td>
-            //     <td>{fish[i].months_str}</td>
-            // </tr>
+            {id: fish[i].fish_name, fish_name: fish[i].fish_name, image: "Image Coming Soon", price: fish[i].price, shadow_size: fish[i].shadow_size, location: fish[i].location, timeRange: timeRange, months: fish[i].months_str}
         )
     }
     return (
@@ -77,6 +62,8 @@ export function formatFishTable (fish) {
             <DataGrid
                 rows={rows}
                 columns={columns}
+                pageSize={50}
+                rowsPerPageOptions={[50]}
             />
         </div>
     )
