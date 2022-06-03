ad870d76cd83b3359829a5613cec662ca5c9bac5
MatthewBloemke <mattbloemke@gmail.com>
2022-06-03 03:01:20 +0000

    moved filter functions to util, added mui

--- src/client/CityFolkDash.jsx
+++ src/client/CityFolkDash.jsx
@@ -1,79 +1,46 @@
 import React, {useEffect, useState} from "react";
 import { asPrettyDate, asTwelveHourTimeString } from "../server/utils/date-time";
-import {formatBugTable, formatFishTable} from "./layout/formatInfoTables"
+import {formatBugTable, formatFishTable} from "./utils/formatInfoTables"
+import { filterTables } from "./utils/filterTables";
+import { AppBar, Grid, Typography } from "@mui/material";
+
 
 const CityFolkDash = ({time, bugs, month, fish}) => {
-    const bugsFilteredByMonth = bugs.filter(bug => bug.months_array.includes(month))
-    const fishFilteredByMonth = fish.filter(single_fish => single_fish.months_array.includes(month))
     const [bugTable, setBugTable] = useState([]);
     const [fishTable, setFishTable] = useState([])
-    const [filterCondition, setFilterCondition] = useState("price")
-    const onChange = (event) => {
-        setFilterCondition(event.target.value)
-    }
 
     useEffect(() => {
-        filterBugsByTime(bugsFilteredByMonth, filterCondition)
-        filterFishByTime(fishFilteredByMonth, filterCondition)
-    }, [filterCondition])
-
-    const filterBugsByTime = (bugs, filterBy) => {
-        const tempBugArr = [];
-        for (let i = 0; i < bugs.length; i++) {
-            if (bugs[i].catch_time_end > bugs[i].catch_time_start) {
-                if (time > bugs[i].catch_time_start && time < bugs[i].catch_time_end) {
-                    tempBugArr.push(bugs[i])
-                }
-            } else {
-                if (time > bugs[i].catch_time_start || time < bugs[i].catch_time_end) {
-                    tempBugArr.push(bugs[i])
-                }
-            }
-        }
-        if (filterBy === "price") {
-            tempBugArr.sort((a, b) => b[filterBy] - a[filterBy])  
-        } else {
-            tempBugArr.sort((a, b) => {
-                return a[filterBy].toLowerCase().localeCompare(b[filterBy].toLowerCase())
-            })
-        }
-        setBugTable(formatBugTable(tempBugArr))
-    }
-    const filterFishByTime = (fish, filterBy) => {
-        const tempFishArr = [];
-        for (let i = 0; i < fish.length; i++) {
-            if (fish[i].catch_time_end > fish[i].catch_time_start) {
-                if (time > fish[i].catch_time_start && time < fish[i].catch_time_end) {
-                    tempFishArr.push(fish[i])
-                }
-            } else {
-                if (time > fish[i].catch_time_start || time < fish[i].catch_time_end) {
-                    tempFishArr.push(fish[i])
-                }
-            }
-        }
-        if (filterBy === "price") {
-            tempFishArr.sort((a, b) => b[filterBy] - a[filterBy])  
-        } else {
-            tempFishArr.sort((a, b) => {
-                return a[filterBy].toLowerCase().localeCompare(b[filterBy].toLowerCase())
-            })
-        }
-        setFishTable(formatFishTable(tempFishArr))
-    }
+        setBugTable(formatBugTable(filterTables(bugs, time, month)))
+        setFishTable(formatFishTable(filterTables(fish, time, month)))
+    }, [])
 
     return (
-        <div>
-            <h1>City Folk</h1>
-            <p>{asPrettyDate()}, {asTwelveHourTimeString(time)}</p>
-            <select name="filter" id="filter" onChange={onChange}>
-                <option value="price">Price</option>
-                <option value="location">Location</option>
-                <option value="bug_name">Bug name</option>
-            </select>
-            {bugTable}
-            {fishTable}
-        </div>
+        <Grid container>
+            <AppBar position="static" sx={{textAlign: "center", pb: "5px", pt: "5px"}}>
+                <Typography variant='h5' component="div" sx={{flexGrow: 1}}>
+                    Animal Crossing: City Folk
+                </Typography>
+            </AppBar>
+            <Grid item xs={12}>
+                <Typography variant='h6' component="div" sx={{textAlign: "center", mt: "15px", mb: "15px"}}>
+                    {asPrettyDate()}, {asTwelveHourTimeString(time)}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} >
+                <Grid container sx={{justifyContent: "center"}}>
+                    <Grid item sx={{width: "80%"}}>
+                        {bugTable}
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} sx={{mt: "15px", mb: "15px"}}>
+                <Grid container sx={{justifyContent: "center"}}>
+                    <Grid item sx={{width: "80%"}}>
+                        {fishTable}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
