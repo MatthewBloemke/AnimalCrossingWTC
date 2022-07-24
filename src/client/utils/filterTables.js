4266a161a204c64de739d9212a2f6bbeba91499c
MatthewBloemke <mattbloemke@gmail.com>
2022-07-23 21:45:15 -0500

    added new filter functions

--- src/client/utils/filterTables.js
+++ src/client/utils/filterTables.js
@@ -15,3 +15,70 @@ export function filterTables (array, time, month) {
     console.log(tempArr)
     return tempArr
 }
+
+export function filter_nh_Tables (array, time, month, hemisphere) {
+    console.log(month)
+    console.log(array)
+    const tempArr = [];
+    for (let i = 0; i < array.length; i++) {
+        const tempItem = array[i];
+        
+        const tempTime = tempItem[hemisphere].times_by_month[month]
+        console.log(`tempTime: ${tempTime}`)
+        if (tempTime.includes('&')) {
+            tempTime = tempTime.split(' & ')
+            for (let j = 0; j < tempTime.length; j++) {
+                tempTime[j] = tempTime[j].split(' – ')
+            }
+        } else if (tempTime.includes('–')) {
+            tempTime = [tempTime.split(' – ')]
+        } else if (tempTime.includes('-')) {
+            tempTime = [tempTime.split(' - ')]
+        } else if (tempTime === 'All day') {
+            tempArr.push(tempItem)
+        }
+
+        const timeItem = [];
+
+        for (let k = 0; k < tempTime.length; k++) {
+            let tempTimeStart = ''
+            let tempTimeEnd = ''
+            if (tempTime != "All day" && tempTime != 'NA') {
+                console.log("k2", tempTime[k])
+                if (tempTime[k][0].includes('AM')) {
+                    tempTimeStart = `${tempTime[k][0][0]}:00`
+                } else if (tempTime[k][0].includes("PM")) {
+                    tempTimeStart = `${Number(tempTime[k][0][0])+12}:00`
+                }
+                if (tempTime[k][1].includes('AM')) {
+                    tempTimeEnd = `${tempTime[k][1][0]}:00`
+                } else if (tempTime[k][1].includes("PM")) {
+                    tempTimeEnd = `${Number(tempTime[k][1][0])+12}:00`
+                }
+                timeItem.push({
+                    catch_time_start: tempTimeStart,
+                    catch_time_end: tempTimeEnd
+                })                
+            } 
+
+        }
+
+        
+        console.log('timeItem')
+        console.log(timeItem)
+        for (let l = 0; l < timeItem.length; l++) {
+            if (timeItem[l].catch_time_end > timeItem[l].catch_time_start) {
+                if (time > timeItem[l].catch_time_start && time < timeItem[l].catch_time_end) {
+                    tempArr.push(tempItem)
+                }
+            } else {
+                if (time > timeItem[l].catch_time_start || time < timeItem[l].catch_time_end) {
+                    tempArr.push(tempItem)
+                }
+            }            
+        }
+
+    }
+    console.log(tempArr)
+    return tempArr
+}
