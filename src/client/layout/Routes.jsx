dd7de3eefdaac1a8a36ce6def5e9ec6214731e5c
MatthewBloemke <mattbloemke@gmail.com>
2022-07-23 21:44:37 -0500

    moved api calls

--- src/client/layout/Routes.jsx
+++ src/client/layout/Routes.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CityFolkDash from '../CityFolkDash';
 import Dashboard from '../Dashboard';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import {today, now, currentMonth} from "../../server/utils/date-time"
 import NewHorizonsDash from '../NewHorizonsDash';
-import { pullNHfish } from '../utils/api';
 const cf_bugs = require("../../server/db/00-cf_bugs.json");
 const cf_fish = require("../../server/db/01-cf_fish.json");
-const nh_n_fish = require("../../server/db/02-nh_n_fish.json")
-pullNHfish()
-    .then(console.log);
+
+
 
 
 const Routes = () => {
+
     return (
         <Switch>
             <Route path="/" exact={true}>
@@ -25,8 +24,8 @@ const Routes = () => {
                 <CityFolkDash date={today()} time={now()} month={currentMonth()} bugs = {cf_bugs} fish = {cf_fish}/>
             </Route>
             <Route path="/newhorizons/:hemisphere">
-                <NewHorizonsDash date={today()} time={now()} month={currentMonth()} fish = {nh_n_fish} bugs = {[]}/>
-            </Route>        
+                <NewHorizonsDash date={today()} time={now()} month={currentMonth()}/>
+            </Route>
         </Switch>
 
     )
